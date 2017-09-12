cecadcd59dddb195da77a4e0444381264133cd41
Angga Wibisono <anggaw@gmail.com>
2017-09-12 10:42:30 +0700

    add error handler on lambda script

--- upload-portfolio-lambda.py
+++ upload-portfolio-lambda.py
@@ -7,20 +7,26 @@ import mimetypes
 
 def lambda_handler(event, context):
     s3 = boto3.resource('s3')
+    sns = boto3.resource('sns')
 
-    portfolio_bucket = s3.Bucket('portfolio.anggawibisono')
-    build_bucket = s3.Bucket('portfoliobuild.anggawibisono')
+    try:
+        topic = sns.Topic('arn:aws:sns:ap-southeast-1:458562924063:deployportfolio')
+        portfolio_bucket = s3.Bucket('portfolio.anggawibisono')
+        build_bucket = s3.Bucket('portfoliobuild.anggawibisono')
 
-    portfolio_zip = StringIO.StringIO()
-    build_bucket.download_fileobj('portfoliobuild.zip',portfolio_zip)
+        portfolio_zip = StringIO.StringIO()
+        build_bucket.download_fileobj('portfoliobuild.zip',portfolio_zip)
 
-    with zipfile.ZipFile(portfolio_zip) as myzip:
-        for nm in myzip.namelist():
-            obj = myzip.open(nm)
-            portfolio_bucket.upload_fileobj(obj,nm,
-                ExtraArgs={'ContentType': mimetypes.guess_type(nm)[0]})
-            portfolio_bucket.Object(nm).Acl().put(ACL='public-read')
+        with zipfile.ZipFile(portfolio_zip) as myzip:
+            for nm in myzip.namelist():
+                obj = myzip.open(nm)
+                portfolio_bucket.upload_fileobj(obj,nm,
+                    ExtraArgs={'ContentType': mimetypes.guess_type(nm)[0]})
+                portfolio_bucket.Object(nm).Acl().put(ACL='public-read')
 
-    print "Job Done!"
-
+        print "Job Done!"
+        topic.publish(Subject="Portfolio Deployed", Message="Portfolio Deployed Successfully")
+    except:
+        topic.publish(Subject="Portfolio Deployed", Message="Portfolio Deployed Failed")
+        raise
     return 'Hello from Lambda'
